02ea1dc6647e15ea56fcef7ead3c543f4cd2a553
github username <github email address>
2018-05-04 11:01:35 -0400

    Supercool

--- app/controllers/superheroes_controller.rb
+++ app/controllers/superheroes_controller.rb
@@ -1,5 +1,39 @@
 class SuperheroesController < ApplicationController
+
+before_action :find_superhero, only: [:show]
+
   def index
     @superheroes = Superhero.all
   end
+
+  def show
+    @superhero
+  end
+
+  def new
+    @superhero = Superhero.new
+    @superpower = Superpower.all
+  end
+
+  def create
+    @superhero = Superhero.create(superhero_params)
+      if @superhero.valid?
+        redirect_to @superhero
+      else
+        redirect_to new_superhero_path
+      end
+  end
+
+
+private
+
+  def find_superhero
+    @superhero = Superhero.find(params[:id])
+  end
+
+  def superhero_params
+    params.require(:superhero).permit(:name, :super_name, :superpower_id)
+  end
+
+
 end
--- app/controllers/superpowers_controller.rb
+++ app/controllers/superpowers_controller.rb
@@ -1,5 +1,28 @@
 class SuperpowersController < ApplicationController
+
+before_action :find_superpower, only: [:show]
+
   def index
     @superpowers = Superpower.all
   end
+
+  def show
+    @superpower 
+  end
+
+
+
+
+
+private
+
+def find_superpower
+  @superpower = Superpower.find(params[:id])
+end
+
+def superpower_params
+  params.require(:superhero).permit(:name, :description)
+end
+
+
 end
